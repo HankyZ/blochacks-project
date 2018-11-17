d10a3ec86482580fe4cc4dbc558142f1ff7004a6
HankyZ <hankyzhang@gmail.com>
2018-11-17 18:23:30 -0500

    implemented post request

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OcrComponent } from './ocr/ocr.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OcrComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
