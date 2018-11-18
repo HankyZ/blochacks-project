a3416d31f0954ce35b640d3c442317d4fc05f819
HankyZ <hankyzhang@gmail.com>
2018-11-18 06:06:24 -0500

    added optimization to reader

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -49,88 +49,96 @@ export class FormComponent implements OnInit {
 
   processCode(lines: any[]) {
     if (!lines || lines.length < 2) {
-      this.codeLine1 = "Unable to read MRZ";
-      this.codeLine2 = "Unable to read MRZ";
+      this.codeLine1 = 'Unable to read MRZ';
+      this.codeLine2 = 'Unable to read MRZ';
       return;
     }
 
-    this.codeLine1 = lines[lines.length - 2].LineText.trim().split('(').join('<').split(' ').join('').toUpperCase();
-    this.codeLine2 = lines[lines.length - 1].LineText.trim().split('(').join('<').split(' ').join('').toUpperCase();
+    this.codeLine1 = lines[lines.length - 2].LineText.trim().split('(').join('<')
+      .split(' ').join('').toUpperCase().split('$').join('S').split('5').join('S')
+      .split('0').join('O').split('1').join('I');
+
+    this.codeLine2 = lines[lines.length - 1].LineText.trim().split('(').join('<')
+      .split(' ').join('').toUpperCase().split('$').join('S');
+
+    this.codeLine2 = this.codeLine2.substring(0, 13) + this.codeLine2.substring(13, 19)
+      .split('O').join('0').split('S').join('5').split('I').join('1').split('\'')
+      .join('1') + this.codeLine2.substring(19, 21) + this.codeLine2.substring(21, 27).split('O')
+        .join('0').split('S').join('5').split('I').join('1') + this.codeLine2
+          .substring(27, this.codeLine2.length);
+
+    // remove all non ascii characters
+    if (this.codeLine2.length > 44)
+      this.codeLine2.replace(/[^\x00-\x7F]/g, '').replace(/‘/g, '');
 
     this.parseAll();
   }
 
   parseAll() {
     if (this.codeLine1 != null) {
-      this.country = this.parseCountry(this.codeLine1);
-      this.lastName = this.parseLastName(this.codeLine1);
-      this.firstName = this.parseFirstName(this.codeLine1);
+      this.country = this.parseCountry();
+      this.lastName = this.parseLastName();
+      this.firstName = this.parseFirstName();
     }
 
     if (this.codeLine2 != null) {
-      this.passportId = this.parsePassId(this.codeLine2);
-      this.dateOfBirth = this.parseDateOfBirth(this.codeLine2);
-      this.sex = this.parseSex(this.codeLine2);
-      this.expirationDate = this.parseExpirationDate(this.codeLine2);
-      this.personalId = this.parsePersonalId(this.codeLine2.substring(28, 44));
+      this.passportId = this.parsePassId();
+      this.dateOfBirth = this.parseDateOfBirth();
+      this.sex = this.parseSex();
+      this.expirationDate = this.parseExpirationDate();
+      this.personalId = this.parsePersonalId();
     }
   }
 
-  parseCountry(code) {
-    var country: string = ((code.split("<"))[1]).substring(0, 3);
-    return country;
+  parseCountry() {
+    return this.codeLine1.split('<')[1].substring(0, 3);
   }
 
-  parseLastName(code) {
-    var list: string = (code.split("<<<")[0]).split("<<")[0];
-    var strLen: number = list.length;
-    var lastNames: string[] = (list.substring(5, strLen)).split("<");
-    var lastName: string = "";
+  parseLastName() {
+    var list: string = (this.codeLine1.split('<<<')[0]).split('<<')[0];
+    var lastNames: string[] = (list.substring(5, list.length)).split('<');
+
+    var lastName: string = '';
 
     for (let i in lastNames)
-      lastName += lastNames[i] + " ";
+      lastName += lastNames[i] + ' ';
 
     return lastName;
   }
 
-  parseFirstName(code) {
-    var firstNames: string[] = (code.split("<<<")[0]).split("<<")[1].split("<");
-    var firstName: string = "";
+  parseFirstName() {
+    var firstNames: string[] = (this.codeLine1.split('<<<')[0]).split('<<')[1].split('<');
+    var firstName: string = '';
 
-    for (let i in firstNames) {
-      firstName += firstNames[i] + " ";
-    }
+    for (let i in firstNames)
+      firstName += firstNames[i] + ' ';
 
     return firstName;
   }
 
-  parsePassId(code) {
-    var passId: string = code.substring(0, 9);
-    var substring: string = "<";
+  parsePassId() {
+    var passId: string = this.codeLine2.substring(0, 9);
     if (passId.substring)
-      passId = (passId.split("<", 1))[0];
+      passId = passId.split('<', 1)[0];
     return passId;
   }
 
-  parseDateOfBirth(code) {
-    var dateOfBirth: string = code.substring(13, 19);
-    return dateOfBirth;
+  parseDateOfBirth() {
+    return this.codeLine2.substring(13, 19);
   }
 
-  parseSex(code) {
-    var sex: string = code.substring(20, 21);
-    return sex;
+  parseSex() {
+    return this.codeLine2.substring(20, 21);
   }
 
-  parseExpirationDate(code) {
-    var date: string = code.substring(21, 27);
-    return date;
+  parseExpirationDate() {
+    return this.codeLine2.substring(21, 27);
   }
 
-  parsePersonalId(code) {
-    if (code.substring(1, 2) != "<")
-      return code.split("<")[0];
+  parsePersonalId() {
+    if (this.codeLine2.charAt(29) != '<')
+      return this.codeLine2.substring(28, 44).split('<')[0];
     else
-      return "N/A";
+      return 'N/A';
   }
 }
